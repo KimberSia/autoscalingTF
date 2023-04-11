b77726e5cd5c9c0863cde4538565f8ea8b4f4528
Kim Sia <kimsia478@gmail.com>
2023-04-11 11:34:04 +0000

    update to code

--- main.tf
+++ main.tf
@@ -52,26 +52,20 @@ resource "aws_launch_template" "wk21-launchtemplate" {
 
   network_interfaces {
     associate_public_ip_address = true
+    security_groups             = [aws_security_group.wk21-secgrp.id]
 
   }
 }
 #create auto scaling group 
 
 resource "aws_autoscaling_group" "week21_asg" {
-  availability_zones        = ["us-east-1a"]
-  desired_capacity          = 2
-  max_size                  = 5
-  min_size                  = 2
-  health_check_grace_period = 300
-  health_check_type         = "ELB"
-  vpc_security_group_ids               = [aws_security_group.wk21-secgrp.id]
-  
+  desired_capacity    = 2
+  max_size            = 5
+  min_size            = 2
+  vpc_zone_identifier = [var.pubsub1-us-east-lb, var.pubsub2-us-east-la]
+
 
   launch_template {
-    id      = aws_launch_template.wk21-launchtemplate.id
-    version = "$Latest"
+    id = aws_launch_template.wk21-launchtemplate.id
   }
 }
-
-
-  
