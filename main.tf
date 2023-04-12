495a020198f354c08396c7c7cbff2f7ccec449eb
Kim Sia <kimsia478@gmail.com>
2023-04-12 19:51:20 +0000

    changes to code

--- main.tf
+++ main.tf
@@ -40,11 +40,11 @@ resource "aws_security_group" "wk21-secgrp" {
 }
 
 resource "aws_autoscaling_group" "wk21asg" {
-  name_prefix        = "wk21asg"
-  max_size           = 5
-  min_size           = 2
-  desired_capacity   = 2
-  availability_zones = var.availability_zones
+  name_prefix         = "wk21asg"
+  max_size            = 5
+  min_size            = 2
+  desired_capacity    = 2
+  vpc_zone_identifier = [var.subnet_id_1, var.subnet_id_2]
   launch_template {
     id = aws_launch_template.wk21-lt-1.id
   }
@@ -53,10 +53,10 @@ resource "aws_autoscaling_group" "wk21asg" {
 
 #create launch template
 resource "aws_launch_template" "wk21-lt-1" {
-  name                   = "wk21-launchtemplate"
-  instance_type          = "t2.micro"
-  image_id               = var.AMI
-  key_name               = var.keyname
-  vpc_security_group_ids = [aws_security_group.wk21-secgrp.id]
-  user_data              = filebase64("${path.root}/apache.sh")
+  name          = "wk21-launchtemplate"
+  instance_type = "t2.micro"
+  image_id      = var.AMI
+  key_name      = var.keyname
+
+  user_data = filebase64("${path.root}/apache.sh")
 }
